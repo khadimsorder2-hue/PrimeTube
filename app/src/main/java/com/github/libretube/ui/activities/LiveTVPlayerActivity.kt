371b8b29f611c6c13f197f1889171df669a91240
PrimeTube Dev <khadimsorder2@users.noreply.github.com>
2026-09-19 21:12:14 +0000

    PrimeTube: add missing AppCompatActivity import in LiveTVPlayerActivity (CI fix)

--- app/src/main/java/com/github/libretube/ui/activities/LiveTVPlayerActivity.kt
+++ app/src/main/java/com/github/libretube/ui/activities/LiveTVPlayerActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
